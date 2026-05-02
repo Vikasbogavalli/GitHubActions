bbbc72ea561445c424f16727eb6c083f1b386feb
Vikasbogavalli <128837784+Vikasbogavalli@users.noreply.github.com>
2026-05-02 21:22:13 +0530

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -5,5 +5,6 @@ public class Hello
     System.out.println("Hello World");
     System.out.println("new line for jenkin trigger");
     System.out.println("new line for jenkin trigger-2");
+     System.out.println("new line for the maven trigger");
   }
 }
